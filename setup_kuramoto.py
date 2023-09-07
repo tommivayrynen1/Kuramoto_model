9f6d8fed12dd6f2e6911645904f1ae9bf9a41b15
Tommi Väyrynen <105491737+tommivayrynen1@users.noreply.github.com>
2023-09-07 13:49:54 +0300

    Add files via upload

---
+++ setup_kuramoto.py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+
+def setup_kuramoto(N, w, K, tspan):
+    def ode(t, theta):
+        theta_dot = w + K/N * np.sum(np.sin(theta - theta[:, np.newaxis]), axis=1)
+        return theta_dot
+
+    thetaI = (np.arange(1, N+1) / N) * 2 * np.pi  # Initial phase starts uniform around unit circle
+
+    sol = solve_ivp(ode, tspan, thetaI, t_eval=np.linspace(tspan[0], tspan[1], 100))
+
+    t = sol.t
+    theta = sol.y
+    complexform = np.exp(1j * theta)
+    order = np.abs(1/N * np.sum(np.exp(1j * theta), axis=0))
+    theta = np.transpose(theta)
+
+    return t, theta, complexform, order
+
+
+
